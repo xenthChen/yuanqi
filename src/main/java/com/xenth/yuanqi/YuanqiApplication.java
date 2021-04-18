a20cdda873b874b8c91cd823647f058eab0c60bf
xenthChen <465059897@qq.com>
2021-04-18 12:09:12 +0800

    Initial commit

---
+++ src/main/java/com/xenth/yuanqi/YuanqiApplication.java
@@ -0,0 +1,13 @@
+package com.xenth.yuanqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuanqiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuanqiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xenth/yuanqi/YuanqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xenth.yuanqi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuanqiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
